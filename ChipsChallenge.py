520a83f76b95cdd173c9404d7477bf6e66d7e248
aiaiBSD <54329973+aiaiBSD@users.noreply.github.com>
2019-12-06 13:07:22 -0700

    Add files via upload

--- ChipsChallenge.py
+++ ChipsChallenge.py
@@ -5,10 +5,11 @@ from pygame.font import *
 with open('CCLvl1.txt') as lvlOne:
     linesOne = [line.split() for line in lvlOne]
 
+with open('CCLvl2.txt') as lvlTwo:
+    linesTwo = [line.split() for line in lvlTwo]
+
 BOARDWIDTH = 9
 BOARDHEIGHT = 9
-MAPHEIGHT = len(linesOne)
-MAPWIDTH = len(linesOne[0])
 TILESIZE = 90
 WINDOWWIDTH = 810
 WINDOWHEIGHT = 900
@@ -29,6 +30,7 @@ ICEBLUE = (165, 242, 243)
 DIRT = (124, 94, 66)
 DIMGRAY = (64, 64, 64)
 ORANGE = (255, 165, 0)
+CHARCOAL = (21, 27, 31)
 
 TILECOLOR = LIGHTGRAY
 TEXTCOLOR = BLACK
@@ -43,8 +45,13 @@ def main():
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     pygame.display.set_caption("Chip's Challenge")
     BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
+    MAPHEIGHT = len(linesOne)
+    MAPWIDTH = len(linesOne[0])
 
     nxtLvl = False
+    levelTwo = False
+    levelTwoX = 3
+    levelTwoY = 9
     mainBoard = []
     for x in range(MAPWIDTH):
         mainBoard.append(linesOne[x])
@@ -58,13 +65,24 @@ def main():
     dead = False
 
     while True:
-        drawBoard(mainBoard, nxtLvl, mapX, mapY, inventory, dead)
+        if nxtLvl:
+            MAPHEIGHT = len(linesTwo)
+            MAPWIDTH = len(linesTwo[0])
+            for x in range(MAPHEIGHT):
+                mainBoard[x] = linesTwo[x]
+            mapY = levelTwoY
+            mapX = levelTwoX
+            for x in range(0, 9):
+                inventory.append("E")
+            nxtLvl = False
+            levelTwo = True
+        drawBoard(mainBoard, mapX, mapY, inventory, dead, MAPWIDTH, MAPHEIGHT)
 
         checkForQuit()
         for event in pygame.event.get():
             if event.type == KEYUP:
                 if event.key in (K_LEFT, K_a) and not dead:
-                    if not mainBoard[mapX - 1][mapY] == "W" and not mainBoard[mapY][mapX - 1] == "R" and not mainBoard[mapY][mapX - 1] == "O" and not mainBoard[mapY][mapX - 1] == "B" and not mainBoard[mapY][mapX - 1] == "P" and not mainBoard[mapY][mapX - 1] == "G":
+                    if not mainBoard[mapY][mapX - 1] == "W" and not mainBoard[mapY][mapX - 1] == "R" and not mainBoard[mapY][mapX - 1] == "O" and not mainBoard[mapY][mapX - 1] == "B" and not mainBoard[mapY][mapX - 1] == "P" and not mainBoard[mapY][mapX - 1] == "G":
                         mapX = mapX - 1
                     elif mainBoard[mapY][mapX - 1] == "R" or mainBoard[mapY][mapX - 1] == "O" or mainBoard[mapY][mapX - 1] == "B" or mainBoard[mapY][mapX - 1] == "P" or mainBoard[mapY][mapX - 1] == "G":
                         go = True
@@ -78,8 +96,15 @@ def main():
                             if i == 8:
                                 go = False
                             i += 1
+                    elif mainBoard[mapY][mapX] == "D" and mainBoard[mapY][mapX - 1] == "W":
+                        shovel = False
+                        for item in inventory:
+                            if item == "h":
+                                shovel = True
+                        if shovel:
+                            mapX = mapX - 2
                 if event.key in (K_RIGHT, K_d) and not dead:
-                    if not mainBoard[mapX + 1][mapY] == "W" and not mainBoard[mapY][mapX + 1] == "R" and not mainBoard[mapY][mapX + 1] == "O" and not mainBoard[mapY][mapX + 1] == "B" and not mainBoard[mapY][mapX + 1] == "P" and not mainBoard[mapY][mapX + 1] == "G":
+                    if not mainBoard[mapY][mapX + 1] == "W" and not mainBoard[mapY][mapX + 1] == "R" and not mainBoard[mapY][mapX + 1] == "O" and not mainBoard[mapY][mapX + 1] == "B" and not mainBoard[mapY][mapX + 1] == "P" and not mainBoard[mapY][mapX + 1] == "G":
                         mapX = mapX + 1
                     elif mainBoard[mapY][mapX + 1] == "R" or mainBoard[mapY][mapX + 1] == "O" or mainBoard[mapY][mapX + 1] == "B" or mainBoard[mapY][mapX + 1] == "P" or mainBoard[mapY][mapX + 1] == "G":
                         go = True
@@ -93,8 +118,15 @@ def main():
                             if i == 8:
                                 go = False
                             i += 1
+                    elif mainBoard[mapY][mapX] == "D" and mainBoard[mapY][mapX + 1] == "W":
+                        shovel = False
+                        for item in inventory:
+                            if item == "h":
+                                shovel = True
+                        if shovel:
+                            mapX = mapX + 2
                 if event.key in (K_UP, K_w) and not dead:
-                    if not mainBoard[mapX][mapY - 1] == "W" and not mainBoard[mapY - 1][mapX] == "R" and not mainBoard[mapY - 1][mapX] == "O" and not mainBoard[mapY - 1][mapX] == "B" and not mainBoard[mapY - 1][mapX] == "P" and not mainBoard[mapY - 1][mapX] == "G":
+                    if not mainBoard[mapY - 1][mapX] == "W" and not mainBoard[mapY - 1][mapX] == "R" and not mainBoard[mapY - 1][mapX] == "O" and not mainBoard[mapY - 1][mapX] == "B" and not mainBoard[mapY - 1][mapX] == "P" and not mainBoard[mapY - 1][mapX] == "G":
                         mapY = mapY - 1
                     elif mainBoard[mapY - 1][mapX] == "R" or mainBoard[mapY - 1][mapX] == "O" or mainBoard[mapY - 1][mapX] == "B" or mainBoard[mapY - 1][mapX] == "P" or mainBoard[mapY - 1][mapX] == "G":
                         go = True
@@ -109,7 +141,7 @@ def main():
                                 go = False
                             i += 1
                 if event.key in (K_DOWN, K_s) and not dead:
-                    if not mainBoard[mapX][mapY + 1] == "W" and not mainBoard[mapY + 1][mapX] == "R" and not mainBoard[mapY + 1][mapX] == "O" and not mainBoard[mapY + 1][mapX] == "B" and not mainBoard[mapY + 1][mapX] == "P" and not mainBoard[mapY + 1][mapX] == "G":
+                    if not mainBoard[mapY + 1][mapX] == "W" and not mainBoard[mapY + 1][mapX] == "R" and not mainBoard[mapY + 1][mapX] == "O" and not mainBoard[mapY + 1][mapX] == "B" and not mainBoard[mapY + 1][mapX] == "P" and not mainBoard[mapY + 1][mapX] == "G":
                         mapY = mapY + 1
                     elif mainBoard[mapY + 1][mapX] == "R" or mainBoard[mapY + 1][mapX] == "O" or mainBoard[mapY + 1][mapX] == "B" or mainBoard[mapY + 1][mapX] == "P" or mainBoard[mapY + 1][mapX] == "G":
                         go = True
@@ -124,12 +156,22 @@ def main():
                                 go = False
                             i += 1
                 if event.key in (K_SPACE, K_x) and dead:
-                    for x in range(MAPWIDTH):
-                        mainBoard[x] = linesOne[x]
+                    if levelTwo == False:
+                        with open('CCLvl1.txt') as lvlOne:
+                            linesThree = [line.split() for line in lvlOne]
+                        for x in range(MAPWIDTH):
+                            mainBoard[x] = linesThree[x]
+                        mapY = (int)(MAPHEIGHT / 2 - 1)
+                        mapX = (int)(MAPWIDTH / 2)
+                    elif levelTwo:
+                        with open('CCLvl2.txt') as lvlTwo:
+                            linesFour = [line.split() for line in lvlTwo]
+                        for x in range(MAPWIDTH):
+                            mainBoard[x] = linesFour[x]
+                        mapY = levelTwoY
+                        mapX = levelTwoX
                     for x in range(0, 9):
                         inventory[x] = "E"
-                    mapY = (int)(MAPHEIGHT / 2 - 1)
-                    mapX = (int)(MAPWIDTH / 2)
                     dead = False
 
         if checkDie(mainBoard[mapY][mapX], inventory):
@@ -147,6 +189,8 @@ def main():
                         go = False
                     i += 1
                 mainBoard[mapY][mapX] = "N"
+            if mainBoard[mapY][mapX] == "w":
+                nxtLvl = True
 
         pygame.display.update()
         FPSCLOCK.tick(FPS)
@@ -162,14 +206,19 @@ def checkForQuit():
 def checkDie(block, invent):
     water = False
     fire = False
+    ice = False
     for item in invent:
         if item == "S":
             water = True
         if item == "f":
             fire = True
+        if item == "i":
+            ice = True
     if block == "H" and not water:
         return True
     if block == "F" and not fire:
+        return True
+    if block == "I" and not ice:
         return True
     return False
 
@@ -177,63 +226,62 @@ def terminate():
     pygame.quit()
     sys.exit()
 
-def drawBoard(board, lvl, x, y, inventory, dead):
+def drawBoard(board, x, y, inventory, dead, MAPWIDTH, MAPHEIGHT):
     DISPLAYSURF.fill(BLACK)
-    if lvl == False:
-        if x <= 3 and y <= 3:
-            xStarting = (int)(4 - x)
-            yStarting = (int)(4 - y)
-            for x in range(0, 9 - xStarting):
-                for y in range(0, 9 - yStarting):
-                    drawTile(board[y][x], y + yStarting, x + xStarting)
-        elif (MAPWIDTH - x) <= 5 and y <= 3:
-            xStarting = (int)(x - 4)
-            yStarting = (int)(4 - y)
-            for x in range(xStarting, MAPWIDTH):
-                for y in range(0, 9 - yStarting):
-                    drawTile(board[y][x], y + yStarting, x - xStarting)
-        elif x <= 3 and (MAPHEIGHT - y) <= 5:
-            xStarting = (int)(4 - x)
-            yStarting = (int)(y - 4)
-            for x in range(0, 9 - xStarting):
-                for y in range(yStarting, MAPHEIGHT - 1):
-                    drawTile(board[y][x], y - yStarting, x + xStarting)
-        elif (MAPWIDTH - x) <= 5 and (MAPHEIGHT - y) <= 5:
-            xStarting = (int)(x - 4)
-            yStarting = (int)(y - 4)
-            for x in range(xStarting, MAPWIDTH):
-                for y in range(yStarting, MAPHEIGHT - 1):
-                    drawTile(board[y][x], y - yStarting, x - xStarting)
-        elif x <= 3:
-            xStarting = (int)(4 - x)
-            yStarting = (int)(y - 4)
-            for x in range(0, 9 - xStarting):
-                for y in range(yStarting, yStarting + 9):
-                    drawTile(board[y][x], y - yStarting, x + xStarting)
-        elif y <= 3:
-            xStarting = (int)(x - 4)
-            yStarting = (int)(4 - y)
-            for x in range(xStarting, xStarting + 9):
-                for y in range(0, 9 - yStarting):
-                    drawTile(board[y][x], y + yStarting, x - xStarting)
-        elif (MAPWIDTH - x) <= 5:
-            xStarting = (int)(x - 4)
-            yStarting = (int)(y - 4)
-            for x in range(xStarting, MAPWIDTH):
-                for y in range(yStarting, yStarting + 9):
-                    drawTile(board[y][x], y - yStarting, x - xStarting)
-        elif (MAPHEIGHT - y) <= 5:
-            xStarting = (int)(x - 4)
-            yStarting = (int)(y - 4)
-            for x in range(xStarting, xStarting + 9):
-                for y in range(yStarting, MAPHEIGHT - 1):
-                    drawTile(board[y][x], y - yStarting, x - xStarting)
-        else:
-            xStarting = (int)(x - 4)
-            yStarting = (int)(y - 4)
-            for x in range(xStarting, xStarting + 9):
-                for y in range(yStarting, yStarting + 9):
-                    drawTile(board[y][x], y - yStarting, x - xStarting)
+    if x <= 3 and y <= 3:
+        xStarting = (int)(4 - x)
+        yStarting = (int)(4 - y)
+        for x in range(0, 9 - xStarting):
+            for y in range(0, 9 - yStarting):
+                drawTile(board[y][x], y + yStarting, x + xStarting)
+    elif (MAPWIDTH - x) <= 5 and y <= 3:
+        xStarting = (int)(x - 4)
+        yStarting = (int)(4 - y)
+        for x in range(xStarting, MAPWIDTH):
+            for y in range(0, 9 - yStarting):
+                drawTile(board[y][x], y + yStarting, x - xStarting)
+    elif x <= 3 and (MAPHEIGHT - y) <= 5:
+        xStarting = (int)(4 - x)
+        yStarting = (int)(y - 4)
+        for x in range(0, 9 - xStarting):
+            for y in range(yStarting, MAPHEIGHT - 1):
+                drawTile(board[y][x], y - yStarting, x + xStarting)
+    elif (MAPWIDTH - x) <= 5 and (MAPHEIGHT - y) <= 5:
+        xStarting = (int)(x - 4)
+        yStarting = (int)(y - 4)
+        for x in range(xStarting, MAPWIDTH):
+            for y in range(yStarting, MAPHEIGHT - 1):
+                drawTile(board[y][x], y - yStarting, x - xStarting)
+    elif x <= 3:
+        xStarting = (int)(4 - x)
+        yStarting = (int)(y - 4)
+        for x in range(0, 9 - xStarting):
+            for y in range(yStarting, yStarting + 9):
+                drawTile(board[y][x], y - yStarting, x + xStarting)
+    elif y <= 3:
+        xStarting = (int)(x - 4)
+        yStarting = (int)(4 - y)
+        for x in range(xStarting, xStarting + 9):
+            for y in range(0, 9 - yStarting):
+                drawTile(board[y][x], y + yStarting, x - xStarting)
+    elif (MAPWIDTH - x) <= 5:
+        xStarting = (int)(x - 4)
+        yStarting = (int)(y - 4)
+        for x in range(xStarting, MAPWIDTH):
+            for y in range(yStarting, yStarting + 9):
+                drawTile(board[y][x], y - yStarting, x - xStarting)
+    elif (MAPHEIGHT - y) <= 5:
+        xStarting = (int)(x - 4)
+        yStarting = (int)(y - 4)
+        for x in range(xStarting, xStarting + 9):
+            for y in range(yStarting, MAPHEIGHT - 1):
+                drawTile(board[y][x], y - yStarting, x - xStarting)
+    else:
+        xStarting = (int)(x - 4)
+        yStarting = (int)(y - 4)
+        for x in range(xStarting, xStarting + 9):
+            for y in range(yStarting, yStarting + 9):
+                drawTile(board[y][x], y - yStarting, x - xStarting)
     funkyBoi = pygame.image.load('FunkyMan.png')
     DISPLAYSURF.blit(funkyBoi, [369, 362])
     i = 0
@@ -476,6 +524,16 @@ def drawTile(block, yPos, xPos):
         pygame.draw.rect(DISPLAYSURF, LIGHTGRAY, (left, top, TILESIZE, TILESIZE))
         door = pygame.image.load('door.png')
         DISPLAYSURF.blit(door, [left, top])
+    if block == "U":
+        pygame.draw.rect(DISPLAYSURF, LIGHTGRAY, (left, top, TILESIZE, TILESIZE))
+        woodDoor = pygame.image.load('woodDoor.png')
+        DISPLAYSURF.blit(woodDoor, [left, top])
+    if block == "s":
+        pygame.draw.rect(DISPLAYSURF, LIGHTGRAY, (left, top, TILESIZE, TILESIZE))
+        pygame.draw.rect(DISPLAYSURF, CHARCOAL, (left + 5, top + 5, TILESIZE * 0.9, TILESIZE * 0.9))
+        pygame.draw.line(DISPLAYSURF, WHITE, (left + TILESIZE * 0.25, top + TILESIZE *0.25), (left + TILESIZE * 0.25, top + TILESIZE *0.75), 4)
+        pygame.draw.line(DISPLAYSURF, WHITE, (left + TILESIZE * 0.75, top + TILESIZE *0.25), (left + TILESIZE * 0.75, top + TILESIZE *0.75), 4)
+        pygame.draw.line(DISPLAYSURF, WHITE, (left + TILESIZE * 0.25, top + TILESIZE *0.5), (left + TILESIZE * 0.75, top + TILESIZE *0.5), 4)
     pygame.draw.line(DISPLAYSURF, DARKGRAY, (left, top), (left + TILESIZE, top), 3)
     pygame.draw.line(DISPLAYSURF, DARKGRAY, (left, top), (left, top + TILESIZE), 3)
     pygame.draw.line(DISPLAYSURF, DARKGRAY, (left + TILESIZE, top), (left + TILESIZE, top + TILESIZE), 3)
